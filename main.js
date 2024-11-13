dbaaeb5cd6eb5c62529138f0e9484dca5627f616
Wert-bity77 <kabulovt707@gmail.com>
2024-11-13 17:39:35 +0500

    main js
    qqqq

---
+++ main.js
@@ -0,0 +1,58 @@
+// number = 0
+// var number = 20
+// console.log(number);
+// let numb = 100
+// numb = 10
+// console.log(numb);
+// const numbers = 15
+// console.log(numbers);
+
+
+// let savol = "sevgi"
+// let savol1 = 'Sabohat'
+
+// console.log(savol);
+
+let yoshi = prompt('Yoshingiz nechida')
+let ismi = prompt('Ismingizni Nima')
+let surname = prompt('Familiyangzni yozing')
+let manzil  = prompt('Yashash joyingizni kiriting')
+let xobbi = prompt('Yoqtirgan ishingizni yozing')
+let kun = prompt(' qaysi kun sizga yoqadi')
+let fasl = prompt('sizga qaysi fasl yoqadi')
+let hafta = prompt('sizga qaysi hafta kunni yoqadi')
+let vaqt = prompt('qachon sdizda bosh vaqt boladi')
+let yoqtirganDars = prompt('maktabda yoqtirgan faningizni yozing')
+let yoqtirmaganDan = prompt('maktabda yoqtirmagan faningizni yozing')
+let coinlar = promp('Marsda qanca coiningiz bor')
+let yonalish = prompt ('Marsda qaysi yonalishni tanladingiz')
+
+
+
+
+
+alert(ismi)
+alert()
+alert()
+alert(vaqt)
+alert(xobbi)
+alert(kun)
+alert(ismfasli)
+alert(fasl)
+alert(hafta)
+alert(yoshi)
+alert(surname)
+alert(coinlar)
+alert(yoqtirganDars)
+alert(yoqtirmaganDan)
+alert(manzil)
+
+
+
+
+
+// console.log(Savol);
+
+// console.log(savol, '+', savol1, 'sevgi');
+
+
